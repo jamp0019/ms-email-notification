5f7e0e3d8bfb60a0578be2b51cca43ae621993cf
jmartinez79 <john1992alex@gmail.com>
2023-11-17 19:51:29 -0500

    Se crea proyecto inicial

---
+++ src/main/java/com/invexdijin/msemailnotification/MsEmailNotificationApplication.java
@@ -0,0 +1,13 @@
+package com.invexdijin.msemailnotification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsEmailNotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsEmailNotificationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/invexdijin/msemailnotification/MsEmailNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.invexdijin.msemailnotification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsEmailNotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
